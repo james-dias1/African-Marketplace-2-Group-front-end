3668b97054a66002b510bdc18b5650186190ee12
james-dias1 <brianbeck115@gmail.com>
2021-10-18 21:44:13 -0700

    products fix

--- african-marketplace/src/components/Products.js
+++ african-marketplace/src/components/Products.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link, useRouteMatch } from 'react-router-dom';
 
+
 import data from '../data'
 import '../CSS/Products.css'
 
@@ -13,6 +14,7 @@ function fetchStock() {
 export default function Products(props) {
     const [items, setItems] = useState([])
     const  url  = useRouteMatch().url;
+    let count = 1;
 
   useEffect(() => {
     fetchStock().then(res => setItems(res.data))
@@ -25,13 +27,13 @@ export default function Products(props) {
                 {/* <h1>products-list</h1> */}
         <div className='container'>
         {items.map(item => (
-            <div className='item-card' key={item.id}>
-                <div className='bg-img'>
+            <div className={'item-card' }  key={item.id}>
+                <div className={'bg-img order'+ (((item.id+1)*2-1)+(item.id%2*(1)))%2 }>
                     {/* 👉 STEP 6 - Link starts, navigates us from <current url> to <current url>/<id of the item> */}
                     <Link to={`${url}/${item.id}`}>
                     <img
                         className='items-list-image'
-                        src="https://picsum.photos/360"
+                        src={item.imageUrl}
                         alt={item.name}
                     />
                     {/* <p>{item.name}</p> */}
@@ -40,7 +42,8 @@ export default function Products(props) {
         
                     {/* <p>${item.price}</p> */}
                 </div>
-                <div className='text'>
+                {/* {(count++)} */}
+                <div className={'text order'+ (((item.id+1)*2)+(item.id%2*(-1)) )%2  }>
                     <h2>{item.name}</h2>
                     <p>{item.instructions}</p>
                     <p>{item.location}</p>
--- african-marketplace/src/data/index.js
+++ african-marketplace/src/data/index.js
@@ -2,29 +2,46 @@
 
 export default [
   {
-    name: 'Reel Viewer',
+    name: 'MAGNA PRIMIS LOBORTIS',
     id: 0,
     price: 29.95,
+    imageUrl:
+    'https://raw.githubusercontent.com/FT-African-Marketplace-2-Group/front-end/products/african-marketplace/src/imgs/pic01.jpeg',
     instructions:
-      "Ah, nostalgia. Give yourself a double dose with this grown-up, just-for-you version of one of childhood's greatest gadgets: the reel viewer. Assemble snapshots of your favorite memories and add them to your very own reel using the redemption code included with your viewer. Once you receive your made-to-order reel, pop it in the viewer and marvel at baby's first steps, or your favorite beach at sunset, or... well, whatever else you want immortalized. Remember: You're the view master now. Made in Oregon.",
+      "Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula",
     location:
       'Republic of Rwanda'
   },
   {
-    name: 'Mathematical Glasses - Set of 4',
+    name: 'TORTOR DOLORE FEUGIAT',
     id: 1,
     price: 38.0,
+    imageUrl:
+       'https://raw.githubusercontent.com/FT-African-Marketplace-2-Group/front-end/products/african-marketplace/src/imgs/pic02.jpeg',
     instructions:
-      "Whether it's advanced calculus or just calculating ideal ice to iced tea ratios, stay hydrated in statistical style with this set. Drinking with mathematical precision is easy: Just use the standard ounce marks and their equivalent constants, then geek out over the expanded formulas—all presented in delightfully differential colors like graph paper blue and #2 pencil gray. Glassware made in New Jersey. Decorated in Blanchester, Ohio. ",
+      "Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.",
     location:
       'Somali Republic'
   },
   {
-    name: 'Shakespearean Insults Chart',
+    name: 'AUGUE ELEIFEND ALIQUET',
     id: 2,
     price: 25.49,
+    imageUrl:
+    'https://raw.githubusercontent.com/FT-African-Marketplace-2-Group/front-end/products/african-marketplace/src/imgs/pic03.jpeg',
     instructions:
-      'We all know someone who deserves to be dubbed a "penurious and indubitate beggar." And from time to time, you may be tempted to say that "he has not so much brain as ear-wax." This rollicking infographic is chock-full of such colorful insults from Shakespeare, the playwright who practically invented the art of the poetic put-down. Designer Tim Sanders culled the Oxford Complete Shakespeare to compile an endlessly entertaining word map of the Bard\'s boisterous, bawdy jabs, each one a mini-masterpiece of raunchy, rancorous Renaissance wit. The four-color, offset-printed poster on 100 lb., acid-free, FSC-certified paper makes the perfect reference and decor for an astute office, library, or thespian lounge. Made in Seattle.',
+      'Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.',
+    location:
+      'Republic of South Africa'
+  },
+  {
+    name: 'AUGUE ELEIFEND ALIQUET',
+    id: 3,
+    price: 16.49,
+    imageUrl:
+    'https://raw.githubusercontent.com/FT-African-Marketplace-2-Group/front-end/products/african-marketplace/src/imgs/pic04.jpeg',
+    instructions:
+      'Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.',
     location:
       'Republic of South Africa'
   },
