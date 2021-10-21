1cf8fb6ad2d3d33813749fcd81acfa5ab4a1ee0f
james-dias1 <brianbeck115@gmail.com>
2021-10-21 10:09:28 -0700

    removed private route

--- african-marketplace/src/components/Header.js
+++ african-marketplace/src/components/Header.js
@@ -11,9 +11,9 @@ const Header = () => {
                 {localStorage.getItem('token') && <Link className="link" to='/products'>Products</Link> }
             </div>
             <div className="right-links">
-                <Link className="AddItem" to='/additem'>Post New Item</Link>
+                <Link className="link" to='/signup'>Signup</Link>
                 <Link className="link" to='/login'>Login</Link>
-                {localStorage.getItem('token') && <Link className="link" to='/postitem'>Post Item</Link> }
+                {localStorage.getItem('token') && <Link className="link" to='/postitem'>Post New Item</Link> }
             </div>
         </nav>
     )
